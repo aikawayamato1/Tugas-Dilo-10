93c0bd2841712429084105926ccd405376e67c2f
DESKTOP-0283LTQ\darkc <dendi.stefanus@binus.ac.id>
2021-10-10 17:58:47 +0700

    Dilo 10 2

---
+++ ProblemSolving Dilo 2/Assets/Move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    float x;
+    float y;
+    
+    Rigidbody2D controller;
+    public float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        controller.AddForce(new Vector2(1, 1));
+        
+    }
+}
